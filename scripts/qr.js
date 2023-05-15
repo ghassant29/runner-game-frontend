5921fbd3cc4f38c460ed55723b5862a82d8a1595
Ghassan El Turk <gass304@gmail.com>
2023-05-15 21:12:56 +0300

    adding control button functionality

--- scripts/qr.js
+++ scripts/qr.js
@@ -1,3 +1,7 @@
 socket.on("switch-to-it", () => {
   window.location.href = "game/index.html";
 });
+const qrImg = document.getElementById("qr-img");
+qrImg.addEventListener("click", () => {
+  window.location.href = "contols/index.html";
+});
