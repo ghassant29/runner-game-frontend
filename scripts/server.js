522abc81169174fe972bd530050dd4c00872c570
Ghassan El Turk <gass304@gmail.com>
2023-05-17 00:37:14 +0300

    fixing enviroment variable

--- scripts/server.js
+++ scripts/server.js
@@ -1,1 +1,4 @@
-const socket = io("https://runner-backend.onrender.com");
+const socketURL = import.meta.env.GASS_SOCKET_SERVER;
+
+export const socket = io(socketURL);
+const socket = io(socketURL);
