6ed0630b87ab507dd70c65a6bbd9f3da4e2b9902
Ghassan El Turk <gass304@gmail.com>
2023-05-16 19:28:05 +0300

    fix

--- game/main.js
+++ game/main.js
@@ -171,6 +171,10 @@ function main() {
         displayMessage(`YOU WIN! YOUR SCORE: ${coins}`);
       } else {
         displayMessage(`YOU LOSE! YOUR SCORE: ${coins}`);
+        socket.emit("gameover-backend");
+        socket.on("game-over", () => {
+          window.location.href = "../index.html";
+        });
       }
       return 0;
     }
